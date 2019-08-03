a67863862c80542dc3dff4e9adf002e006a2ea4a
Gabriel Queiroz <gabrielqueirozzn@gmail.com>
2019-08-03 17:52:35 -0300

    Extrair  arquivo com componente de loading das rotas

---
+++ src/routes/LoadingRoute.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import LoadingComponent from '../components/Loading';
+
+export const Loading = ({ error, retry, pastDelay }) => {
+  if (error) {
+    return (
+      <div>
+        Error!
+        <button type="button" onClick={retry}>
+          Retry
+        </button>
+      </div>
+    );
+  }
+  if (pastDelay) {
+    return <LoadingComponent />;
+  }
+  return null;
+};
+
+export const LoadingDashboard = ({ error, retry, pastDelay }) => {
+  if (error) {
+    return (
+      <div>
+        Error!
+        <button type="button" onClick={retry}>
+          Retry
+        </button>
+      </div>
+    );
+  }
+  if (pastDelay) {
+    return <LoadingComponent isDashboard />;
+  }
+  return null;
+};
