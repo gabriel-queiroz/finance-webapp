43559316884882d94771b7d791c55546caabe922
Gabriel Queiroz <gabrielqueirozzn@gmail.com>
2019-08-03 17:53:36 -0300

    Configurar reactotron para melhorar o debug da aplicacao

---
+++ src/config/reactotron.js
@@ -0,0 +1,14 @@
+import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+import sagaPlugin from 'reactotron-redux-saga';
+
+const reactotron = Reactotron.configure()
+  .use(sagaPlugin())
+  .use(reactotronRedux())
+  .connect();
+
+console.tron = reactotron;
+
+console.log('entrou');
+
+export default reactotron;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './config/reactotron';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
