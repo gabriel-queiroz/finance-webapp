5ac7ed2fc3caf1b6cedabfb0583fd1c24b51b18c
Gabriel Queiroz <gabrielqueirozzn@gmail.com>
2019-08-10 11:18:58 -0300

    Adicionar o inputCurrency no componente HOC de field

--- src/components/CreateAntFields/CreateAntFields.js
+++ src/components/CreateAntFields/CreateAntFields.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {
-  DatePicker, Form, Input, TimePicker, Select,
+  DatePicker, Form, Input, InputNumber, TimePicker, Select,
 } from 'antd';
+import InputCurrency from '../InputCurrency';
 
 const FormItem = Form.Item;
 const { Option } = Select;
@@ -16,21 +17,24 @@ const CreateAntField = AntComponent => ({
   type,
   ...props
 }) => {
+  let onInputChange;
   const touched = form.touched[field.name];
   const submitted = submitCount > 0;
   const hasError = form.errors[field.name];
   const submittedError = hasError && submitted;
   const touchedError = hasError && touched;
-  const onInputChange = ({ target: { value } }) => form.setFieldValue(field.name, value);
+  if (field.name === 'value') {
+    onInputChange = value => form.setFieldValue(field.name, value);
+  } else {
+    onInputChange = ({ target: { value } }) => form.setFieldValue(field.name, value);
+  }
   const onChange = value => form.setFieldValue(field.name, value);
   const onBlur = () => form.setFieldTouched(field.name, true);
   return (
     <div className="field-container">
       <FormItem
         label={label}
-        hasFeedback={
-          !!((hasFeedback && submitted) || (hasFeedback && touched))
-        }
+        hasFeedback={!!((hasFeedback && submitted) || (hasFeedback && touched))}
         help={submittedError || touchedError ? hasError : false}
         validateStatus={submittedError || touchedError ? 'error' : 'success'}
       >
@@ -53,4 +57,6 @@ const CreateAntField = AntComponent => ({
 export const AntSelect = CreateAntField(Select);
 export const AntDatePicker = CreateAntField(DatePicker);
 export const AntInput = CreateAntField(Input);
+export const AntInputNumber = CreateAntField(InputNumber);
 export const AntTimePicker = CreateAntField(TimePicker);
+export const AntInputCurrency = CreateAntField(InputCurrency);
